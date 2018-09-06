f358c7ff7220b472f6b6bcf466903d3a26bea953
Stephen Ouyang <xisouyang@gmail.com>
2018-09-06 16:20:54 -0700

    added mongodb and Review Model

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 // app.get('/', (req, res) => {
 //   res.render('home', { msg: 'Suh Dude'});
 // })
 
 //mock array of objects
-let reviews = [
-    { title: "review #1" },
-    { title: "A bitter disappointment -Mom" }
-]
+// let reviews = [
+//     { title: "review #1" },
+//     { title: "A bitter disappointment -Mom" },
+//     { title: "Best guy ever -Me" }
+// ]
+
+//model of actual array of objects
+const Review = mongoose.model('Review', {
+    title: String
+});
 
 //index
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews});
+    Review.find()
+      .then(reviews => {
+          res.render('reviews-index', { reviews: reviews })
+      })
+      .catch(err => {
+          console.log(err);
+      });
 });
 
 app.listen(3000, () => {
