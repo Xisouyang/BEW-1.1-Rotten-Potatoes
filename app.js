cd6a408e9664b58f90bac4d2f4309d7099c628f9
Stephen Ouyang <xisouyang@gmail.com>
2018-09-05 17:18:16 -0700

    users can see reviews

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Suh Dude'});
+// })
+
+//mock array of objects
+let reviews = [
+    { title: "review #1" },
+    { title: "A bitter disappointment -Mom" }
+]
+
+//index
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Suh Dude'});
-})
+    res.render('reviews-index', { reviews: reviews});
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
