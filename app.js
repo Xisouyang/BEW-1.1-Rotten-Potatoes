771ef31333ad86e30923ee47da837294703a2202
Stephen Ouyang <xisouyang@gmail.com>
2018-09-05 17:03:56 -0700

    added basic web templates and files

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.send('Suh Dude')
+  res.render('home', { msg: 'Suh Dude'});
 })
 
 app.listen(3000, () => {
