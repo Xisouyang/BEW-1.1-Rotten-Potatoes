f30aed15f31e40424ad02e7b3b301cc82aa678cc
Stephen Ouyang <xisouyang@gmail.com>
2018-09-07 16:23:26 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -46,14 +46,23 @@ app.get('/reviews/new', (req, res) => {
     res.render('reviews-new', {})
 });
 
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', {review: review})
+    }).catch((err) => {
+        console.log(err.message);
+    })
+    // res.send('I\'m a review');
+});
+
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then(( review ) => {
         console.log(review);
-        res.redirect('/');
+        res.redirect(`/reviews/${review._id}`);
     }).catch((err) => {
         console.log(err.message);
     });
-    console.log(req.body);
+    // console.log(req.body);
     //res.render('reviews-new', {})
 });
 
